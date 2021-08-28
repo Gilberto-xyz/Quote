e5cbc588a5c7e1b6d0e68fa6ba33d1e1f11a562e
Gilberto Nava Marcos-W <gilberto.nava.marcos@outlook.com>
2021-08-28 17:52:56 -0500

    First commit

---
+++ quoteAPI.js
@@ -0,0 +1,16 @@
+const content = document.getElementById("quote");
+const author = document.getElementById("author");
+
+let url = "https://api.quotable.io/random";
+async function getData(){
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    let quote = data.content;
+    let writer = data.author;
+
+    content.textContent = '" ' + quote + ' "';
+    author.textContent = writer;
+}
+
+getData();
