4456468701b04a96179904abf315f1fe335291e6
Gilberto Nava Marcos-W <gilberto.nava.marcos@outlook.com>
2021-08-28 17:57:35 -0500

    new font

--- quoteAPI.js
+++ quoteAPI.js
@@ -2,10 +2,10 @@ const content = document.getElementById("quote");
 const author = document.getElementById("author");
 
 let url = "https://api.quotable.io/random";
-async function getData(){
+async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    
+
     let quote = data.content;
     let writer = data.author;
 
